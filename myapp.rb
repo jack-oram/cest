e63ee16a3ae7ef0bcdac3cc57d59a77f1df01707
jack-oram <50764842+jack-oram@users.noreply.github.com>
2019-05-27 19:46:15 +1200

    email name
    wip

--- myapp.rb
+++ myapp.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   erb :index, :layout => :layout
 end
+
+
+get '/register' do
+  erb :register, :layout => :layout
+end
+
+
+post '/register' do
+  @post = params
+  "hello #{@post}"
+end

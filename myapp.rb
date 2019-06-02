e550cc778e308a9d95e8e8912cbac016b356a38c
jack-oram <50764842+jack-oram@users.noreply.github.com>
2019-06-03 11:16:57 +1200

    connect to database

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
+require "active_record"
+require "mysql2"
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql2",
+  :host     => "127.0.0.1",
+  :username => "jack",
+  :password => "scooter",
+  :database => "cest"
+)
+
+class User < ActiveRecord::Base
+end
+
+ActiveRecord::Migration.create_table :users do |t|
+  t.string :first_name
+  t.string :last_name
+  t.string :email
+  t.string :password
+end
 
 get '/' do
   erb :index, :layout => :layout

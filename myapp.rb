688ea0beec9c1cbcf2dd0185a8e143f958e4c4d6
jack-oram <50764842+jack-oram@users.noreply.github.com>
2019-06-03 12:05:54 +1200

    register saved onto database

--- myapp.rb
+++ myapp.rb
@@ -12,14 +12,19 @@ ActiveRecord::Base.establish_connection(
 )
 
 class User < ActiveRecord::Base
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :terms, acceptance: true
 end
 
-ActiveRecord::Migration.create_table :users do |t|
-  t.string :first_name
-  t.string :last_name
-  t.string :email
-  t.string :password
-end
+# ActiveRecord::Migration.create_table :users do |t|
+#   t.string :first_name
+#   t.string :last_name
+#   t.string :email
+#   t.string :password
+# end
+
+
 
 get '/' do
   erb :index, :layout => :layout
@@ -32,6 +37,20 @@ end
 
 
 post '/register' do
-  @post = params
-  "hello #{@post}"
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  email = params[:email]
+  password = params[:password]
+
+  puts "SAVE"
+  @user = User.new(first_name: first_name, 
+                   last_name: last_name, 
+                   email: email, 
+                   password: password)
+  if @user.save
+    "saved ok"
+  else
+    puts @user.inspect
+    "errors #{@user}"
+  end
 end
